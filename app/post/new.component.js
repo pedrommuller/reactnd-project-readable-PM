961c3b4700d4d83838bf428368905844bec6629d
Pedro Muller <me@pedromuller.com>
2017-10-06 09:17:16 -0600

    form new post

--- app/post/new.component.js
+++ app/post/new.component.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
+import Guid from 'guid'
 
 import Badge from '../shared/badge.component'
 
@@ -9,19 +10,60 @@ class New extends React.Component {
     super(props);
     this.props = props;
     this.postQuestion = this.postQuestion.bind(this);
-    this.handleTextchange = this.handleTextchange.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.validateForm = this.validateForm.bind(this);
+    this.state ={
+      category:'',
+      path:'',
+      body:'',
+      title:''
+    }
+  }
+
+  validateForm(){
+    return !(
+      this.state.body==='' ||
+      this.state.title==='' ||
+      this.state.category==='')
   }
 
   postQuestion(){
-    alert('post question');
+    if(this.validateForm()){
+      const post = {
+        id:Guid.raw(),
+        timestamp:+new Date(),
+        author: this.props.currentUser,
+        ...this.state
+      }
+      console.log(post);
+    }
   }
 
-  handleTextchange(e){
-    console.log(e);
+  handleChange(e, control){
+    if(control==='category'){
+      const {options, selectedIndex} = e.target;
+      if(options[selectedIndex].value===''){
+        this.setState({
+          category:'',
+          path:''
+        })
+      }else{
+        this.setState({
+          category:options[selectedIndex].text,
+          path:options[selectedIndex].value
+        })
+      }
+
+    }else{
+      this.setState({
+        [control]:e.target.value
+      })
+    }
+
   }
 
   render() {
-    const {visible,user} = this.props;
+    const {visible,user, categories} = this.props;
     const style ={};
     style.display = visible?'block':'none';
 
@@ -31,8 +73,17 @@ class New extends React.Component {
           <span onClick={this.props.close} className="close">&times;</span>
           <Badge color={user.color} initials={user.initials} name={user.name} />
           <p>
-            <textarea onChange={(e)=>this.handleTextchange(e)}>
+            {this.state.title ==='' && <span className="warning">*Title required</span>}
+            <input type="text" placeholder="What do you have in mind?"
+              onChange={(e)=>this.handleChange(e,'title')} />
+            {this.state.body==='' && <span className="warning">*Description required</span>}
+            <textarea onChange={(e)=>this.handleChange(e,'body')}>
             </textarea>
+            {this.state.category==='' && <span className="warning">*Category required</span>}
+            <select onChange={(e)=>this.handleChange(e,'category')}>
+              <option value="" key="1"> - Select -</option>
+              {categories.map((e)=><option key={e.path} value={e.path}>{e.name}</option>)}
+            </select>
             <a onClick={(e)=>this.postQuestion(e)} className="pure-button pure-button-primary align-right">
               Post question
             </a>
@@ -45,7 +96,9 @@ class New extends React.Component {
 
 function mapStateToProps(state){
   return {
-    user:state.users.list[state.users.current]
+    user:state.users.list[state.users.current],
+    currentUser:state.users.current,
+    categories:state.categories.list
   }
 }
 
